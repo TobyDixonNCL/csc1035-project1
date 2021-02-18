2a0fad3d3bad822dc27fb6d516ff77a459c762bc
Toby Dixon <thetjdixon1234@gmail.com>
2021-02-18 17:17:12 +0000

    Added Getters and Setters and relevant methods

--- src/com/company/District.java
+++ src/com/company/District.java
@@ -1,5 +1,65 @@
 package com.company;
 
-public class District {
+import java.util.ArrayList;
+import java.util.List;
 
+public class District {
+    private String localDistrict;
+    private Incident[] burglaryIncidents;
+
+    public String getLocalDistrict() {
+        return localDistrict;
+    }
+
+    public void setLocalDistrict(String localDistrict) {
+        this.localDistrict = localDistrict;
+    }
+
+    public Incident[] getBurglaryIncidents() {
+        return burglaryIncidents;
+    }
+
+    public void setBurglaryIncidents(Incident[] burglaryIncidents) {
+        this.burglaryIncidents = burglaryIncidents;
+    }
+
+    public Incident highestValue(){
+        Incident currentInc = new Incident();
+
+        for (Incident inc: burglaryIncidents){
+            if (inc.getValue() > currentInc.getValue()){
+                currentInc = inc;
+            }
+        }
+
+        return currentInc;
+    }
+
+    public int avgIncValue(){
+        int totalVal = 0;
+
+        for (Incident inc: burglaryIncidents){
+            totalVal += inc.getValue();
+        }
+
+        return (totalVal / burglaryIncidents.length);
+
+    }
+
+    public List<Incident> incAboveValue(float cost){
+        List<Incident> aboveVal = new ArrayList<>();
+
+        for (Incident inc: burglaryIncidents){
+
+            if (inc.getValue() > cost){
+                aboveVal.add(inc);
+            }
+
+        }
+
+        return aboveVal;
+
+    }
 }
+
+
--- src/com/company/Incident.java
+++ src/com/company/Incident.java
@@ -1,5 +1,41 @@
 package com.company;
 
 public class Incident {
+    private int value;
+    private String postcode;
+    private String month;
+    private int year;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
 
 }
