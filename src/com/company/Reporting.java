1e771f505bfb26b33f9cfc4f00479f4afecf28f3
Toby Dixon <thetjdixon1234@gmail.com>
2021-02-18 17:27:58 +0000

    Added variables, Getters and Setters, and relevant methods.

--- src/com/company/Reporting.java
+++ src/com/company/Reporting.java
@@ -1,7 +1,61 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Reporting {
 
+    public District[] districts;
+
+    public District[] getDistricts() {
+        return districts;
+    }
+
+    public void setDistricts(District[] districts) {
+        this.districts = districts;
+    }
+
+    public District largestAverageValue(){
+
+        District currentDist = new District();
+
+        for (District district: districts){
+
+            if (district.avgIncValue() > currentDist.avgIncValue()){
+                currentDist = district;
+            }
+
+        }
+
+        return currentDist;
+
+    }
+
+    public Incident largestEver(){
+
+        Incident currentInc = new Incident();
+
+        for (District district: districts){
+            if (district.highestValue().getValue() > currentInc.getValue()){
+                currentInc = district.highestValue();
+            }
+        }
+
+        return currentInc;
+
+    }
+
+    public List<Incident> incAboveValue(float value){
+
+        List<Incident> allInc = new ArrayList<>();
+
+        for (District district: districts){
+            allInc.addAll(district.incAboveValue(value));
+        }
+
+        return allInc;
+
+    }
 
 
 }
